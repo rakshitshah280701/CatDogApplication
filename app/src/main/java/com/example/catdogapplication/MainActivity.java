069f1ac77867678f99eb3ad74d59063340d62bfa
rakshitshah280701 <rakshitshah14@gmail.com>
2024-10-07 12:39:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/catdogapplication/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.catdogapplication;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+import com.example.catdogapplication.ml.Model;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgView;
+    private Button select, predict;
+    private TextView tv;
+    private Bitmap img;
+
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = (ImageView) findViewById(R.id.imageView);
+        tv = (TextView) findViewById(R.id.textView);
+        select = (Button) findViewById(R.id.button);
+        predict = (Button) findViewById(R.id.button2);
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Uri uri = result.getData().getData();
+                        imgView.setImageURI(uri);
+                        try {
+                            img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                //startActivityForResult(intent, 100);
+                activityResultLauncher.launch(intent);
+
+
+            }
+        });
+
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                img = Bitmap.createScaledBitmap(img, 150, 150, true);
+
+                try {
+                    Model model = Model.newInstance(getApplicationContext());
+
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 150, 150, 3}, DataType.FLOAT32);
+                    TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+                    tensorImage.load(img);
+                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+                    inputFeature0.loadBuffer(byteBuffer);
+
+                    // Runs model inference and gets result.
+                    Model.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    // Releases model resources if no longer used.
+                    model.close();
+
+                    float[] outputArray = outputFeature0.getFloatArray();
+                    int dogOutput = outputArray[0] > 0 ? 1 : 0;
+                    int catOutput = outputArray[1] > 0 ? 1 : 0;
+
+                    // Display the results in the TextView
+                    tv.setText("Cat: " + dogOutput + "\nDog: " + catOutput);
+                } catch (IOException e) {
+                    // TODO Handle the exception
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100)
+        {
+            imgView.setImageURI(data.getData());
+
+            Uri uri = data.getData();
+            try {
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
